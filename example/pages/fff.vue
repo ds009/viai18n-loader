c54bd4fbfc7f1231770a69b5866fc05990a57cb6
dengshendengshen <dengshendengshen@gmail.com>
2018-12-17 16:50:48 +0800

    feat: handle expression in template

---
+++ example/pages/fff.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-index">
+    <Header></Header>
+    <h1>需要翻译的内容</h1>
+    <p>测试一下{{test+'本同人本乳糖'}}</p>
+  </div>
+</template>
+
+<script>
+  import Header from '../components/Header'
+
+  export default {
+    components: {Header},
+    computed: {
+      test() {
+        return '测试'
+      },
+    },
+  }
+</script>
+
